208af6252d9d728a538e1e8b5fd80ff69a26e861
edobasky <edore7@gmail.com>
2022-01-16 23:47:12 +0100

    initial commit

---
+++ StoneGridDemo/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoneGridDemo.Data;
+using StoneGridDemo.Models;
+using System.Collections.Generic;
+
+namespace StoneGridDemo.Controllers
+{
+
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> catItem = _db.Categories;
+            return View(catItem);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
